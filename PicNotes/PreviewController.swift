bf5920a668a1f174ebf320561126fe9b308ed486
Allan Araujo <allan7araujo@gmail.com>
2018-07-26 06:57:59 -0400

    Refreshing project. Hero wasn't what I needed for the PreviewController.Playing around with some other stuff. Deleted the Base class and transition class becuase thats not the most effiecient/smart way of handling funcitonality I want.

--- PicNotes/PreviewController.swift
+++ PicNotes/PreviewController.swift
@@ -7,8 +7,36 @@
 //
 
 import UIKit
+import Speech
+import Hero
 
-class PreviewController: PreviewControllerBase {
+class PreviewController: UIViewController {
+    
+    let audioCaptureService = AudioCaptureService()
+    let notesId = "notedId"
+    var notes = ""
+    
+    var previewImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFill
+        return iv
+    }()
+    
+    let cancelButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "cancel_shadow").withRenderingMode(.alwaysOriginal), for: .normal)
+        button.addTarget(self, action: #selector(handleCancel), for: .touchUpInside)
+        return button
+    }()
+    
+    
+    let microphoneButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.addTarget(self, action: #selector(handleRecord), for: .touchUpInside)
+        button.setImage(#imageLiteral(resourceName: "muted-white").withRenderingMode(.alwaysOriginal), for: .normal)
+        button.isEnabled = false
+        return button
+    }()
     
     private let notesButton: UIButton = {
         let button = UIButton(type: .system)
@@ -17,27 +45,63 @@ class PreviewController: PreviewControllerBase {
         button.addTarget(self, action: #selector(handleOpenNotes), for: .touchUpInside)
         return button
     }()
-    
+        
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationController?.isNavigationBarHidden = true
+        
+        audioCaptureService.enableSpeechRecognition()
+        
+        setupLayout()
+        
+    }
+    
+    fileprivate func setupLayout() {
+        view.addSubview(previewImageView)
+        previewImageView.anchor(top: view.topAnchor, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
+        
+        view.addSubview(microphoneButton)
+        microphoneButton.anchor(top: view.topAnchor, left: nil, bottom: nil, right: view.rightAnchor, paddingTop: 24, paddingLeft: 0, paddingBottom: 0, paddingRight: 12, width: 32, height: 32)
+        
+    
+        view.addSubview(cancelButton)
+        cancelButton.anchor(top: view.topAnchor, left: view.leftAnchor, bottom: nil, right: nil, paddingTop: 24, paddingLeft: 12, paddingBottom: 0, paddingRight: 0, width: 50, height: 50)
+        
         view.addSubview(notesButton)
         notesButton.anchor(top: nil, left: nil, bottom: view.bottomAnchor, right: view.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 50, paddingRight: 12, width: 50, height: 50)
         
-        notesButton.hero.isEnabled = true
-        notesButton.hero.id = "heroId"
+        microphoneButton.addTarget(self, action: #selector(handleRecord), for: .touchUpInside)
     }
     
-    @objc func handleOpenNotes(){
-        let pnc = PreviewNotesController()
-        pnc.previewImageView.image = self.previewImageView.image
-
-        pnc.hero.isEnabled = true
-        pnc.contentView.hero.id = "heroId"
-        
-        pnc.contentView.hero.modifiers = [.scale()]
-        pnc.contentView.backgroundColor = .white
-        
-        pnc.hero.modalAnimationType = .auto
-        present(pnc, animated: true, completion: nil)
+    override func viewDidLayoutSubviews() {
+        if audioCaptureService.recordingEnabled() {
+            print("audio enabled...")
+            microphoneButton.setImage(#imageLiteral(resourceName: "microphone-full-white").withRenderingMode(.alwaysOriginal), for: .normal)
+            microphoneButton.isEnabled = true
+        }
     }
+    
+    
+    @objc func handleOpenNotes(){
+        if audioCaptureService.recordingEnabled() {
+            //Open notes view and start recording
+        }
+    }
+    
+    @objc func handleRecord() {
+        print("record button pressed")
+        
+    }
+    
+    @objc func handleCancel() {
+        print("Cancel button pressed")
+        //self.dismiss(animated: true, completion: nil
+        self.view.window!.rootViewController?.dismiss(animated: false, completion: nil)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        //textView.resignFirstResponder()
+    }
+    
 }
